37d708138168456774f39f64ebc7ff4cefc8277d
hiepnguyen6014 <hiepnguyen6014@gmail.com>
2023-06-17 16:09:41 +0700

    chore: update withAuth hoc

--- src/hocs/withAuth.tsx
+++ src/hocs/withAuth.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useEffect } from "react";
+/* import React, { FC, useEffect } from "react";
 import { Navigate } from "react-router-dom";
 
 import Loading from "~/components/common/Loading";
@@ -36,6 +36,43 @@ const withAuth = (Component: FC) => {
   };
 
   return MyComponent;
+};
+
+export default withAuth; */
+
+import React, { FC, useEffect } from "react";
+import { Navigate } from "react-router-dom";
+import Loading from "~/components/common/Loading";
+
+import { checkCookieToken, useAppDispatch, useAppSelector } from "~/redux";
+import { ASYNC_STATUS } from "~/redux";
+
+const withAuth = (Component: FC) => {
+  const AuthenticatedComponent = () => {
+    const user = useAppSelector((state) => state.login);
+    const dispatch = useAppDispatch();
+
+    useEffect(() => {
+      if (user.status === ASYNC_STATUS.IDLE) {
+        dispatch(checkCookieToken());
+      }
+    }, [dispatch, user.status]);
+
+    if (
+      user.status === ASYNC_STATUS.IDLE ||
+      user.status === ASYNC_STATUS.LOADING
+    ) {
+      return <Loading />;
+    }
+
+    if (user.status === ASYNC_STATUS.FAILED) {
+      return <Navigate to="/login" replace />;
+    }
+
+    return <Component />;
+  };
+
+  return AuthenticatedComponent;
 };
 
 export default withAuth;
