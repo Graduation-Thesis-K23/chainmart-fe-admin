c967d9a37b9dc148f6807d1496daa8d04323e1d3
hiepnguyen6014 <hiepnguyen6014@gmail.com>
2023-07-13 23:11:27 +0700

    feat: new aws

--- Dockerfile
+++ Dockerfile
@@ -12,7 +12,6 @@ COPY . .
 
 RUN yarn build
 
-
 FROM nginx:1.13.12-alpine
 COPY --from=build /app/build /usr/share/nginx/html
 COPY nginx.conf /etc/nginx/nginx.conf
--- cypress.config.ts
+++ cypress.config.ts
@@ -1,11 +1,7 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
 import { defineConfig } from "cypress";
 
 export default defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
     video: true,
     videoCompression: 32,
     baseUrl: "http://localhost:8080",
